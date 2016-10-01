eee2b140e76520a571817285216e6ae253b11f0b
Francisco Mendoza Sánchez <PacoMS95@users.noreply.github.com>
2016-10-01 15:20:21 +0200

    Add files via upload

---
+++ Light_sensor_one_analog_other_digital.ino
@@ -0,0 +1,28 @@
+//we define macros, so when we say "redLED" we are talking about "11"
+#define blueLED 2
+
+void setup() {
+  // We set the LED's as output and the light sensor as an input
+  pinMode (blueLED, OUTPUT);
+  //We set the communication between serial port and the PC
+  Serial.begin(9600);
+  
+}
+
+void loop() {
+int LEDLuminosity = 0;
+//here we set the up and down of the led
+for (LEDLuminosity = 0; LEDLuminosity < 255; LEDLuminosity++)
+  {
+    Serial.println(LEDLuminosity);
+    analogWrite (blueLED, LEDLuminosity);
+    delay(10);
+  }
+
+for (LEDLuminosity = 255; LEDLuminosity > 0; LEDLuminosity--)
+  {
+    Serial.println(LEDLuminosity);
+    analogWrite (blueLED, LEDLuminosity);
+    delay(10);
+  }
+}
